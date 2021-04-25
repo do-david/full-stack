988ebff46adf48026aa74aa0666370f572d05179
do-david <50655142+do-david@users.noreply.github.com>
2021-04-25 23:04:12 +0200

    fix tmp babel issue

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
-import userRouter from "./users.route";
-import orderRouter from "./orders.route";
-import productRouter from "./products.route";
+const userRouter = require('./users.route');
+const orderRouter = require('./orders.route');
+const productRouter = require('./products.route');
 import express from "express";
 const router = express.Router();
 
